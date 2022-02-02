3e660e075466c7f073f6058af281b3f2d24218d6
andre_araujo_dev <andreearaujo98@gmail.com>
2022-02-01 21:34:41 -0300

    writing more two tests in sumTwoNumbers function

--- server/lib/calculator.js
+++ server/lib/calculator.js
@@ -1,3 +1,19 @@
 module.exports.sumTwoNumbers = (numberOne, numberTwo) => {
-  return numberOne + numberTwo;
+
+  // The plus signal before 'numberOne' and 'numberTwo' convert string value to integer
+  // like parseInt(n) and Number(n),
+  // If receive a empty string '+' convert it to zero and parseInt to NaN;
+
+  return forceValueToBeAInteger(numberOne) + forceValueToBeAInteger(numberTwo);
+}
+
+const forceValueToBeAInteger = (value) => {
+  
+  value = parseInt(value, 10);
+
+  if(Number.isNaN(value)){
+    throw new Error('Please check your input');
+  }
+
+  return value;
 }
--- server/lib/calculator.spec.js
+++ server/lib/calculator.spec.js
@@ -3,3 +3,25 @@ const { sumTwoNumbers } = require('./calculator');
 it('should sum 2 and 2 and the result must be 4', () => {
   expect(sumTwoNumbers(2,2)).toBe(4);
 });
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4 ', () => {
+  expect(sumTwoNumbers('2','2')).toBe(4);
+});
+
+it('should throw and error if what is provided to the method cannot be summed', () => {
+  expect(() => {
+    sumTwoNumbers('','2');
+  }).toThrowError();
+
+  expect(() => {
+    sumTwoNumbers([2,2])
+  }).toThrowError();
+  
+  expect(() => {
+    sumTwoNumbers({})
+  }).toThrowError();
+  
+  expect(() => {
+    sumTwoNumbers()
+  }).toThrowError();
+});
